5f2f9e1e39a38947b54bcba75ecbb1a7e6abd05d
albina_kazartseva <albinak74@gmail.com>
2025-02-27 21:57:21 +0500

    homework_15_1

--- src/category.py
+++ src/category.py
@@ -10,6 +10,7 @@ class Category:
     products: list
     category_count = 0
     product_count = 0
+    quantity = 0
 
     def __init__(self, name: str, description: str, products: list):
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
@@ -17,23 +18,36 @@ class Category:
         self.description = description
         self.__products = products if products else []
         self.category_count += 1
-        self.product_count += len(self.__products)
+        self.product_count += len(self.__products) if products else 0
+        for product in self.__products:
+            self.quantity += product.quantity
 
-    def __repr__(self):
+    def __str__(self) -> str:
+        """Магический метод для строкового представления информации об объекте класса"""
+        return f"{self.name}, количество продуктов: {self.quantity} шт."
+
+    def __repr__(self) -> str:
         """Метод для создания 'официального' строкового представления объекта"""
         return f"Category(name={self.name}, description={self.description}, products={self.products})"
 
-    def add_product(self, product: Product):
-        """Метод для добавления и подсчета товара"""
-        self.__products.append(product)
-        Category.product_count += 1
-
     @property
-    def products(self):
+    def products(self) -> str:
         """Геттер возвращает возможность просмотра приватного списка товаров"""
         products_list = []
         for product in self.__products:
-            products_list.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
+            products_list.append(str(product))
         return "\n".join(products_list)
 
+    @property
+    def products_in_list(self) -> list:
+        """Геттер возвращает возможность просмотра приватного списка товаров типа list для итерации"""
+        return self.__products
 
+    def get_products(self) -> list:
+        """Возвращает приватный атрибут __products."""
+        return self.__products
+
+    def add_product(self, product: Product) -> int:
+        """Метод для добавления и подсчета товара"""
+        self.__products.append(product)
+        Category.product_count += 1
---
+++ src/product_iterator.py
@@ -0,0 +1,42 @@
+from src.category import Category
+from src.product import Product
+
+
+class ProductIterator:
+    """Вспомогательный класс, с помощью которого можно перебирать товары одной категории"""
+
+    def __init__(self, category_obj):
+        if not isinstance(category_obj.products_in_list, list):
+            raise ValueError("Список продуктов должен быть list")
+        self.category = category_obj
+        self.index = 0
+
+    def __iter__(self):
+        """Возвращает итератор."""
+        self.index = 0
+        return self
+
+    def __next__(self):
+        """Магический метод возвращает следующий товар из списка товаров категории"""
+        if self.index < len(self.category.products_in_list):
+            product = self.category.products_in_list[self.index]
+            self.index += 1
+            return product
+        else:
+            raise StopIteration("Нет больше продуктов")
+
+
+if __name__ == "__main__":
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+    iterator = ProductIterator(category1)
+    for product in iterator:
+        print(product)
